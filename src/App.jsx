81b315ab9f694d1780ef4ae4a0569f2db7d88167
IsroilIslomov37 <islomovisroil37@gmail.com>
2026-06-30 19:04:51 +0500

    final code

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,15 @@
-import { useState } from 'react'
+import Header from './Header';
+import Main from './Main';
+import Footer from './Footer';
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-    
-    </>
-  )
+    <div className="flex flex-col min-h-screen">
+      <Header />
+      <Main />
+      <Footer />
+    </div>
+  );
 }
 
-export default App
+export default App;
