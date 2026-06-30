0661ddd77f25208405d800ca02031a36a086ef7f
IsroilIslomov37 <islomovisroil37@gmail.com>
2026-06-30 19:02:34 +0500

    Footer

---
+++ src/assets/components/Footer.jsx
@@ -0,0 +1,7 @@
+export default function Footer() {
+    return (
+        <footer className="bg-slate-900 text-slate-400 p-8 text-center border-t border-slate-800">
+            <p>© 2026 GamerGear. Все права защищены.</p>
+        </footer>
+    );
+}
